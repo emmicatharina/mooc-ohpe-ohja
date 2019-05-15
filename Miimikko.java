2583e02ca7817c6db8ac0a028180ec42635da42e
emmicatharina <43492887+emmicatharina@users.noreply.github.com>
2019-05-15 10:36:02 +0300

    Add files via upload

---
+++ Miimikko.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author emmisulander
+ */ 
+public class Miimikko {
+    
+    private String nimi;
+    
+    public Miimikko(String nimi) {
+        this.nimi = nimi;
+    }
+    
+    public String getNimi() {
+        return this.nimi;
+    }
+    
+}
---
+++ Sopraano.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author emmisulander
+ */
+public class Sopraano extends Henkilo implements Laulava {
+    
+    public Sopraano(String nimi) {
+        super(nimi);
+    }
+
+    public void laula(String laulu) {
+
+    }
+
+}
